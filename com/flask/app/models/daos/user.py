3021fc7a74c3786c826543ed80183d15a155bda3
bac-ta <entropydb@gmail.com>
2021-07-19 12:37:18 +0700

    update

--- com/flask/app/models/daos/user.py
+++ com/flask/app/models/daos/user.py
@@ -4,7 +4,7 @@ Class user that mapping with "user" table
 
 from settings import *
 
-db = db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 
 
 class User(db.Model):
@@ -14,16 +14,8 @@ class User(db.Model):
     password = db.Column(db.String(20), nullable=False)
     type = db.Column(db.String(10), nullable=False)
 
-    def __init__(self, username, password, type):
-        self.username = username
-        self.password = password
-        self.type = type
+    def add_user(self, username, password, type):
+        user = User(username=username, password=password, type=type)
+        db.session.add(user)
+        db.session.commit()
 
-    def get_username(self):
-        return self.username
-
-    def get_password(self):
-        return self.password
-
-    def get_type(self):
-        return self.type
---
+++ com/flask/app/models/rests/requests/user_request.py
@@ -0,0 +1,5 @@
+class UserRequest(object):
+    def __init__(self, username, password, type):
+        self.username = username
+        self.password = password
+        self.type = type
